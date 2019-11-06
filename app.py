33be46577a624da3153bb3e9a2ab963a43805c76
Marek Lovčí <mareklovci@gmail.com>
2019-11-06 16:03:23 +0100

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from lifelines.utils import datetimes_to_durations
 from utils import start_end_for_diagnosis
 
 cols = [
-    'Věk', 'Zařazen od', 'Datum vzniku příznaku', 'Datum 1. hosp.', 'Datum zahájení trombolýzy',
+    'Pohlaví', 'Věk', 'Zařazen od', 'Datum vzniku příznaku', 'Datum 1. hosp.', 'Datum zahájení trombolýzy',
     'Datum zahájení trombektomie', 'Lokalizace tr.', 'Etiolog. klas.', 'TSS příjem', 'TSS prop.', 'MRS Před', 'MRS',
     'MRS 3M', 'mRS-out-3M', 'mRS - 1Y', 'zpracování 0/1/2 ', 'mRS-out', 'BI 1 rok', 'ADL 1 rok', 'datum +',
     'Unnamed: 25', 'událost', 'Diag. hypertenze', 'Diag. diabetes', 'Diag. hyperlip.', 'Akt. kuřák', 'Bývalý kuřák',
@@ -29,8 +29,15 @@ diagnosis_list = [
 
 
 def kaplan_meier_analysis(df, rows=2, columns=3):
+    """
+    Kaplan-Meier estimates for individual diagnoses
+
+    :param df: Pandas dataframe with data
+    :param rows: number of rows for subplots
+    :param columns: number of columns for subplots
+    :return: Plots the data
+    """
     fig, axes = plt.subplots(columns, rows, figsize=[6, 10])
-    # fig.suptitle('Kaplan-Meier estimates for individual diagnoses')
 
     for pos, diagnosis in enumerate(diagnosis_list):
         timeline = np.linspace(0, 21)
@@ -48,7 +55,7 @@ def kaplan_meier_analysis(df, rows=2, columns=3):
         kmf.fit(t, event_observed=e, timeline=timeline)
 
         # get the plot position
-        ax: plt.Axes = axes[pos % 3, pos % 2]
+        ax: plt.Axes = axes[pos % columns, pos % rows]
 
         # plot Kaplan-Meier
         kmf.plot(ax=ax)
@@ -63,6 +70,12 @@ def kaplan_meier_analysis(df, rows=2, columns=3):
     fig.show()
 
 
+def plot_logistic_regression(data_x, data_y):
+    data = {'x': data_x, 'y': data_y}
+    sns.regplot(x='x', y='y', data=data, logistic=True, n_boot=100)
+    plt.show()
+
+
 def main():
     # read excel sheet
     df = pd.read_excel('data.xlsx')
@@ -74,7 +87,7 @@ def main():
     df.drop(df[df['mRS-out'] == 7].index, inplace=True)  # those are not relevant according to the assignment
 
     # drop columns
-    df.drop(columns=['ID1', 'ID2', 'ID3'], inplace=True)  # drop ID columns
+    df.drop(columns=['ID1', 'ID3'], inplace=True)  # drop ID columns
     df.drop(columns=['Obec', 'Okres'], inplace=True)  # drop location columns
     df.drop(columns=['datum kontroly', 'pobyt', 'pobyt 1'], inplace=True)  # drop additional information columns
 
@@ -83,21 +96,22 @@ def main():
     df['TSS prop.'] = df['TSS prop.'].fillna(0)  # replace NaN with 0
     df['TSS prop.'] = df['TSS prop.'].astype(int)  # make this column's values numerical
 
+    # Age to mRS comparison
+    plot_logistic_regression(df['Věk'], df['mRS-out'])
+
+    # TSS to mRS comparison
+    plot_logistic_regression(df['TSS příjem'], df['mRS-out'])
+
+    # TSS difference to mRS comparison
+    difference = abs(df['TSS příjem'] - df['TSS prop.'])
+    plot_logistic_regression(difference, df['mRS-out'])
+
+    # Sex to mRS comparison
+    sex = [0 if i is 'F' else 1 for i in df['Pohlaví']]
+    plot_logistic_regression(np.asarray(sex), df['mRS-out'])
+
     # plot kaplan-meier curves
     kaplan_meier_analysis(df)
-
-    data = {'x': df['Věk'], 'y': df['mRS-out']}
-    sns.regplot(x='x', y='y', data=data)
-    plt.show()
-
-    data = {'x': df['TSS příjem'], 'y': df['mRS-out']}
-    sns.regplot(x='x', y='y', data=data)
-    plt.show()
-
-    difference = abs(df['TSS prop.'] - df['TSS příjem'])
-    data = {'x': difference, 'y': df['mRS-out']}
-    sns.regplot(x='x', y='y', data=data)
-    plt.show()
 
 
 if __name__ == '__main__':
